f95adfe206ff51d0c056ab05482821fb5c6d3cfe
Taranvir Waraich <taranvir@me.com>
2017-04-14 11:00:54 -0400

    Started calibration submodule

---
+++ visiontracker/include/visiontracker/calibration.h
@@ -0,0 +1,14 @@
+/*
+ * Calibration Header
+ * This submodule will ease camera calibration
+ *
+ * Copyright (c) 2017, EchoVision
+ *
+ * Contributors:
+ *   - Taranvir Waraich
+ */
+
+#ifndef CALIBRATION_H
+#define CALIBRATION_H
+
+#endif
---
+++ visiontracker/src/calibration.cpp
@@ -0,0 +1,11 @@
+/*
+ * Source for "visiontracker/calibration.h"
+ *
+ * Copyright (c) 2017, EchoVision
+ *
+ * Contributors:
+ *   - Taranvir Waraich
+ */
+
+
+#include "visiontracker/calibration.h"
