691ebee4bc462e47f99363010c88f8d2a5c48e89
Taranvir Waraich <taranvir@me.com>
2017-04-15 00:44:10 -0400

    Started calibration

--- visiontracker/include/visiontracker/calibration.h
+++ visiontracker/include/visiontracker/calibration.h
@@ -11,4 +11,34 @@
 #ifndef CALIBRATION_H
 #define CALIBRATION_H
 
+#include <string>
+#include <vector>
+#include <math.h>
+
+#include <opencv2/core.hpp>
+
+
+// Defining real dimensions in meters
+#define SQUAREDIMENSION 0.025f
+
+class Serializable {
+public:
+    virtual void write(cv::FileStorage& fs) const = 0;
+    virtual void read(const cv::FileNode& fs) = 0;
+    virtual void setDefault() = 0;
+};
+
+class Settings : public Serializable {
+protected:
+  cv::Size boardSize;
+  float squareSize;
+
+public:
+  void write(cv::FileStorage& fs) const;
+  void read(const cv::FileNode& node);
+  void setDefault();
+
+  cv::Size getBoardSize();
+};
+
 #endif
--- visiontracker/src/calibration.cpp
+++ visiontracker/src/calibration.cpp
@@ -9,3 +9,24 @@
 
 
 #include "visiontracker/calibration.h"
+
+/* Settings */
+
+void Settings::write(cv::FileStorage& fs) const {
+  fs << "{" << "boardSize" << boardSize
+     << "squareSize" << squareSize << "}";
+}
+
+void Settings::read(const cv::FileNode& node) {
+  node["boardSize"] >> boardSize;
+  node["squareSize"] >> squareSize;
+}
+
+void Settings::setDefault() {
+  boardSize = cv::Size(9,6);
+  squareSize = SQUAREDIMENSION;
+}
+
+cv::Size Settings::getBoardSize() {
+  return boardSize;
+}
