4c66238547766c60792feb9f0e137abb1c10a7e9
Taranvir Waraich <taran@Taranvirs-Mac-Pro.local>
2017-04-22 14:51:47 -0400

    Added logger

---
+++ visiontracker/include/visiontracker/logger.h
@@ -0,0 +1,15 @@
+/*
+ * Logger Header
+ * This submodule will ease creating logs
+ *
+ * Copyright (c) 2017, EchoVision
+ *
+ * Contributors:
+ *   - Taranvir Waraich
+ */
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+
+#endif
---
+++ visiontracker/src/logger.cpp
@@ -0,0 +1,8 @@
+/*
+ * Source for "visiontracker/logger.h"
+ *
+ * Copyright (c) 2017, EchoVision
+ *
+ * Contributors:
+ *   - Taranvir Waraich
+ */
