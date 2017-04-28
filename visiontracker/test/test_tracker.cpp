cc7f27c5a8741c826faf42d8485b23632387e350
Taranvir Waraich <taranvir@me.com>
2017-04-27 22:15:26 -0400

    Started tests for tracker however they can be better organized

---
+++ visiontracker/test/test_tracker.cpp
@@ -0,0 +1,15 @@
+#include "visiontracker/tracker.h"
+#include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
+
+using namespace vt;
+
+TEST(ArucoDetectorTestCase, Test) {
+
+}
+
+int main(int argc, char ** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
